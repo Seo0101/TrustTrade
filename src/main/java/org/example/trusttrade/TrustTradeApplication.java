c1fb263af972046b282fc23b0628b33ad63765a8
seohyunjin <tjguswls10414@gmail.com>
2025-04-17 19:45:50 +0900

    first commit

---
+++ src/main/java/org/example/trusttrade/TrustTradeApplication.java
@@ -0,0 +1,13 @@
+package org.example.trusttrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrustTradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/trusttrade/TrustTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trusttrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
